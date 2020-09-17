65a77b68bd4e344964464bc24a723858e1563f0b
Tuomo Artama <tuomo.artama@gmail.com>
2020-09-17 12:54:51 +0300

    ohjelman rakennetta parannettu, testejä kirjoitettu

--- regex-tulkki/src/main/java/regextulkki/Main.java
+++ regex-tulkki/src/main/java/regextulkki/Main.java
@@ -5,6 +5,7 @@
  */
 package regextulkki;
 
+import domain.Translator;
 /**
  *
  * @author tuomoart
@@ -19,37 +20,12 @@ public class Main {
         String syote = "aaabbbbcccc";
         String lauseke = "a*b*c*";
         
-        char[] syoteChars = syote.toCharArray();
-        char[] lausekeChars = lauseke.toCharArray();
+        Translator tulkki = new Translator();
         
-        boolean kuuluu = true;
-        
-        int lausekeI = 0;
-        int syoteI = 0;
-        
-        while (lausekeI<lausekeChars.length && syoteI<syoteChars.length) {
-            if (lausekeChars[lausekeI]=='*') {
-                while (syoteI<syoteChars.length && syoteChars[syoteI]==lausekeChars[lausekeI-1]) {
-                    syoteI++;
-                }
-                lausekeI++;
-                continue;
-            }
-            
-            if (syoteChars[syoteI]!=lausekeChars[lausekeI]) {
-                kuuluu = false;
-                break;
-            }
-            
-            lausekeI++;
-            syoteI++;
+        if (tulkki.matches(syote, lauseke)) {
+            System.out.println("Kuuluu");
+        } else {
+            System.out.println("Ei kuulu");
         }
-        
-        //Jos jompaa kumpaa ei käyty loppuun, erityisesti syötettä niin ei täsmännyt, paitsi jos lauseke loppui kesken mutta viimeinen merkki oli operaattori
-        if (syoteI<syoteChars.length || (lausekeI<lausekeChars.length && lausekeChars[lausekeChars.length-1]!='*')) {
-            kuuluu = false;
-        }
-
-        System.out.println(kuuluu);
     }
 }
