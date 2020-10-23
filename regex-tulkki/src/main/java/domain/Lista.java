101d2d0efe591162099041c6ca9744ff6ae869d5
Tuomo Artama <tuomo.artama@gmail.com>
2020-10-23 12:55:42 +0300

    Lista lisätty, koodi toimivaa

---
+++ regex-tulkki/src/main/java/domain/Lista.java
@@ -0,0 +1,50 @@
+package domain;
+
+/**
+ *
+ * @author tuomoart
+ */
+public class Lista {
+    private Merkkijono[] lista;
+    private int koko;
+    
+    public Lista() {
+        lista = new Merkkijono[1];
+        koko = 0;
+    }
+    
+    public Integer size() {
+        return koko;
+    }
+    
+    public void add(Merkkijono m) {
+        if (this.taynna()) {
+            this.tuplaa();
+        }
+        
+        lista[koko] = m;
+        koko++;
+    }
+    
+    public Merkkijono get(int i) {
+        if (i > koko) {
+            return new Merkkijono();
+        }
+        
+        return lista[i];
+    }
+    
+    private void tuplaa() {
+        Merkkijono[] temp = new Merkkijono[lista.length*2];
+        
+        for (int i = 0; i < lista.length; i++) {
+            temp[i] = lista[i];
+        }
+        
+        lista = temp;
+    }
+    
+    private boolean taynna() {
+        return koko == lista.length;
+    }
+}
